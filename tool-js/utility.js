5008ac89eb95ed9ef4d85da1f15a40df25657518
adrian shiu <linca30mus@gmail.com>
2017-01-08 17:53:26 +0800

    test flashcard

--- tool-js/utility.js
+++ tool-js/utility.js
@@ -11,3 +11,24 @@ function qs(k) {
     }
     return "";
 }
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+	function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
