b50b064cf66f2382dc133b397d45a6591ed17b08
perrot <linca30mus@gmail.com>
2018-09-09 18:35:13 +0800

    1.Add the project "¨¦ºq«÷­µ¿é¤Jªk½m²ßï " to Projects page.

---
+++ apps/google_pinyin_input_method_exercise_book/js/consonant.js
@@ -0,0 +1,4 @@
+﻿var consonant=["ㄅ","ㄆ","ㄇ","ㄈ","ㄉ","ㄊ","ㄋ","ㄌ","ㄍ","ㄎ","ㄏ","ㄐ","ㄑ","ㄒ","ㄓ","ㄔ","ㄕ","ㄖ","ㄗ","ㄘ","ㄙ"];
+    var consonantAns=["b","p","m","f","d","t","n","l","g","k","h","j","q","x","zh","ch","sh","r","z","c","s"];
+var _data=consonant;
+var _ans=consonantAns;
