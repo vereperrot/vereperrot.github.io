5ebf1abb4b47dc2f42964dc3354c79df86d4fcce
perrot <linca30mus@gmail.com>
2019-08-11 22:53:51 +0800

    [+] update interval-alarm app

---
+++ apps/interval-alarm/js/main.js
@@ -0,0 +1,82 @@
+var _mainTimer;
+var _flashTimer;
+const TITLE_TEXT="Interval Alarm";
+const INTERVAL_SECONDS = "IntervalSeconds";
+const ENABLE_SOUND="EnableSound";
+var _totalSeconds=0;
+var _flashMessage="";
+
+function startTimer() {
+	var start=document.getElementById("btnStart");
+	var stop=document.getElementById("btnStop");
+
+	enableButton(stop,start);
+	clearInterval(_flashTimer);
+	_totalSeconds=0;
+	updateTime(_totalSeconds.toString().toHHMMSS());
+	_mainTimer = setInterval(main, 1000);
+}
+
+function updateTime(text){
+	var elm=document.getElementById("timeDisplay");
+	elm.innerText=text;
+}
+
+function enableButton(enableBtn, disableBtn){
+    disableBtn.setAttribute("class","w3-btn w3-round-xxlarge w3-black w3-block w3-disabled");
+    enableBtn.setAttribute("class","w3-btn w3-round-xxlarge w3-black w3-block");
+}
+
+function stopTimer() {
+	var start=document.getElementById("btnStart");
+	var stop=document.getElementById("btnStop");
+	
+	enableButton(start,stop);
+    clearInterval(_mainTimer);
+   
+    _flashMessage=document.getElementById("timeDisplay").innerText;
+    _flashTimer = setInterval(flash, 500);
+}
+
+function flash(){
+	var elm=document.getElementById("timeDisplay");
+	elm.innerText=_flashMessage;
+}
+
+function main() {
+	var enableSound=localStorage.getItem(ENABLE_SOUND);
+	var intervalSecs=parseInt(localStorage.getItem(INTERVAL_SECONDS));
+    _totalSeconds++;
+    updateTime(_totalSeconds.toString().toHHMMSS());
+    if(_totalSeconds%intervalSecs==0)
+    {
+        if(enableSound){
+            var x = document.getElementById("myAudio"); 
+            x.play(); 
+        }
+    }
+}
+
+String.prototype.toHHMMSS = function () {
+	var sec_num = parseInt(this, 10); // don't forget the second param
+	var hours   = Math.floor(sec_num / 3600);
+	var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+	var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+	if (hours   < 10) {hours   = "0"+hours;}
+	if (minutes < 10) {minutes = "0"+minutes;}
+	if (seconds < 10) {seconds = "0"+seconds;}
+	return hours+':'+minutes+':'+seconds;
+}
+
+function initial(){
+	
+	updateTime('00:00:00');
+	if(localStorage.getItem(INTERVAL_SECONDS)==null){
+		localStorage.setItem(INTERVAL_SECONDS, 30);
+	}
+	if(localStorage.getItem(ENABLE_SOUND)==null){
+		localStorage.setItem(ENABLE_SOUND, true);
+	}
+}
+initial();
---
+++ apps/interval-alarm/sw.js
@@ -0,0 +1,41 @@
+var cacheName = 'interval-alarm-pwa';
+var filesToCache = [
+  '/',
+  '/apps/interval-alarm/index.html',
+  '/apps/interval-alarm/about.html',
+  '/apps/interval-alarm/settings.html',
+  '/apps/interval-alarm/css/font-awesome.min.css',
+  '/apps/interval-alarm/css/font-family-allerta-stencil.css',
+  '/apps/interval-alarm/css/HTx0L209KT-LmIE9N7OR6eiycOe1_Db2.woff2',
+  '/apps/interval-alarm/css/w3.css',
+  '/apps/interval-alarm/fonts/w3.css',
+  '/apps/interval-alarm/fonts/fontawesome-webfont.eot',
+  '/apps/interval-alarm/fonts/fontawesome-webfont.svg',
+  '/apps/interval-alarm/fonts/fontawesome-webfont.ttf',
+  '/apps/interval-alarm/fonts/fontawesome-webfont.woff',
+  '/apps/interval-alarm/fonts/fontawesome-webfont.woff2',
+  '/apps/interval-alarm/js/main.js',
+  '/apps/interval-alarm/snd/alarm.mp3',
+  '/apps/interval-alarm/snd/alarm.ogg',
+  '/apps/interval-alarm/snd/alarm.wav'
+];
+//console.log('in sw.js');
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+        //console.log('install');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+        //console.log('fetch');
+      return response || fetch(e.request);
+    })
+  );
+});
