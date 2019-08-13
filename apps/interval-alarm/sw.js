1bef108e9d7ee9f4ea58b88cf826ec1529818d22
vereperrot <linca30mus@gmail.com>
2019-08-13 10:36:44 +0800

    [+] Update sw.js of interval-alarm pwa app

--- apps/interval-alarm/sw.js
+++ apps/interval-alarm/sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'interval-alarm-pwa';
+var cacheName = 'interval-alarm-pwa-1';
 var filesToCache = [
   '/',
   '/apps/interval-alarm/index.html',
@@ -30,20 +30,53 @@ var filesToCache = [
 //console.log('in sw.js');
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-  e.waitUntil(
+  /*e.waitUntil(
     caches.open(cacheName).then(function(cache) {
         //console.log('install');
       return cache.addAll(filesToCache);
     })
-  );
+  );*/
+	e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  )
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+/*self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
         //console.log('fetch');
       return response || fetch(e.request);
     })
   );
-});
+});*/
+
+self.addEventListener('fetch',function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(response){
+      if(response != null){
+        return response
+      }
+      return fetch(e.request.url)
+    })
+  )
+})
+self.addEventListener('activate',function(e){
+  e.waitUntil(
+    //get all cache name
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        // get all cache content diff from current version cache
+        cacheNames.filter(cacheNames => {
+          return cacheNames !== cacheStorageKey
+        }).map(cacheNames => {
+          return caches.delete(cacheNames)
+        })
+      )
+    }).then(() => {
+      return self.clients.claim()
+    })
+  )
+})
