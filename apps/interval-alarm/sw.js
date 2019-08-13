00fe46d25e8a1ce2f132750188a46ad557bd75b4
vereperrot <linca30mus@gmail.com>
2019-08-13 11:04:32 +0800

    [+] Update sw.js of interval-alarm pwa app

--- apps/interval-alarm/sw.js
+++ apps/interval-alarm/sw.js
@@ -1,8 +1,22 @@
 var cacheName = 'interval-alarm-pwa';
 var filesToCache = [
   '/',
-  '/apps/interval-alarm/index.html'
-
+  '/apps/interval-alarm/index.html',
+  '/apps/interval-alarm/about.html',
+  '/apps/interval-alarm/settings.html',
+  '/apps/interval-alarm/css/font-awesome.min.css',
+  '/apps/interval-alarm/css/font-family-allerta-stencil.css',
+  '/apps/interval-alarm/css/HTx0L209KT-LmIE9N7OR6eiycOe1_Db2.woff2',
+  '/apps/interval-alarm/css/w3.css',
+  '/apps/interval-alarm/fonts/fontawesome-webfont.eot',
+  '/apps/interval-alarm/fonts/fontawesome-webfont.svg',
+  '/apps/interval-alarm/fonts/fontawesome-webfont.ttf',
+  '/apps/interval-alarm/fonts/fontawesome-webfont.woff',
+  '/apps/interval-alarm/fonts/fontawesome-webfont.woff2',
+  '/apps/interval-alarm/js/main.js',
+  '/apps/interval-alarm/snd/alarm.mp3',
+  '/apps/interval-alarm/snd/alarm.ogg',
+  '/apps/interval-alarm/snd/alarm.wav'
 ];
 //console.log('in sw.js');
 /* Start the service worker and cache all of the app's content */
